66db0dc08480b0319c8b83292bc96f626dcde071
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2016-07-20 20:40:04 -0300

    Bootstrap bug fixes

--- src_angular/Farm_Dashboard/src/main.ts
+++ src_angular/Farm_Dashboard/src/main.ts
@@ -3,9 +3,13 @@ import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
 import { DashboardComponent } from './app/dashboard';
 import { appRouterProviders }   from './app/app.routes';
+import { HTTP_PROVIDERS } from '@angular/http';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  appRouterProviders,
+  HTTP_PROVIDERS
+]);
